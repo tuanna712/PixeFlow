ebee21a9eb7131e444e116b1f70875bf88e349e8
tuanna712 <tuanna712@gmail.com>
2025-08-19 00:11:38 -0600

    update

--- prep/postgre_conn.py
+++ prep/postgre_conn.py
@@ -202,10 +202,7 @@ class PostgresDB:
             self.cursor.execute("SELECT frame_index FROM btc_frame WHERE video_id=%s", (video_id,))
             return [row[0] for row in self.cursor.fetchall()]
         processed_frame_indexes = get_frame_indexes_by_video_id(frame_data[1])
-        print(frame_data[2], processed_frame_indexes)
-        if len(processed_frame_indexes) > 0:
-            print(type(frame_data[2]), type(processed_frame_indexes[0]))
-        if frame_data[2] in processed_frame_indexes:
+        if int(frame_data[2]) in processed_frame_indexes:
             print(f"Frame with video_id '{frame_data[1]}' and frame_index '{frame_data[2]}' already exists. Skipping insert.")
         else:
             print("Add new one!")
